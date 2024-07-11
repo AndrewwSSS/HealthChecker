ac42188019d9483faa04ca790c487eed6835e2fb
AndrewwSSS <clash2clans1one@gmail.com>
2024-07-11 10:58:30 +0300

    refactor ajax/views

--- ajax/urls.py
+++ ajax/urls.py
@@ -1,20 +1,21 @@
 from django.urls import path
 
-from ajax.views import (change_password,
-                        update_user,
-                        add_power_exercise,
-                        delete_exercise,
-                        delete_approach,
-                        add_approach, delete_training)
+from ajax.views import (UpdatePasswordView,
+                        UpdateUser,
+                        AddPowerExerciseView,
+                        AddApproachView,
+                        DeleteTrainingView,
+                        DeleteApproach,
+                        DeleteExerciseView)
 
 urlpatterns = [
-    path("user_update", update_user, name="user_update"),
-    path("change_password", change_password, name="change_password"),
-    path("add_power_training_exercise", add_power_exercise, name="add_power_training_exercise"),
-    path("delete_exercise", delete_exercise, name="delete_exercise"),
-    path("delete_approach", delete_approach, name="delete_approach"),
-    path("add_approach", add_approach, name="add_approach"),
-    path("trainings/delete_training", delete_training, name="delete_training"),
+    path("user_update", UpdateUser.as_view(), name="user_update"),
+    path("change_password", UpdatePasswordView.as_view(), name="change_password"),
+    path("add_power_training_exercise", AddPowerExerciseView.as_view(), name="add_power_training_exercise"),
+    path("delete_exercise", DeleteExerciseView.as_view(), name="delete_exercise"),
+    path("delete_approach", DeleteApproach.as_view(), name="delete_approach"),
+    path("add_approach", AddApproachView.as_view(), name="add_approach"),
+    path("trainings/delete_training", DeleteTrainingView.as_view(), name="delete_training"),
 ]
 
 app_name = 'ajax'
--- ajax/views.py
+++ ajax/views.py
@@ -1,19 +1,21 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, JsonResponse
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import get_object_or_404
+from django.views import View
 
 from ajax.forms import UserUpdateForm
 from main.models import (Exercise,
                          Approach,
-                         PowerTrainingExercise, Training, CyclingTraining, SwimmingTraining, Walk, Jogging)
+                         PowerTrainingExercise,
+                         CyclingTraining,
+                         SwimmingTraining,
+                         Walk,
+                         Jogging,
+                         PowerTraining,
+                         User)
 
-INVALID_METHOD_RESPONSE = JsonResponse(
-    {
-        "status": "error"
-    },
-    status=405
-)
 
 SUCCESS_RESPONSE = JsonResponse({
     "status": "success",
@@ -28,176 +30,130 @@ INVALID_DATA_RESPONSE = JsonResponse({
 }, status=422)
 
 
-@login_required
-def change_password(request: HttpRequest) -> JsonResponse:
-    if request.method != "POST":
+class UpdatePasswordView(LoginRequiredMixin, View):
+    def post(self, request: HttpRequest) -> JsonResponse:
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return JsonResponse({
+                "status": "success",
+            }, status=200)
+
+        return JsonResponse({
+            "status": "error",
+            "errors": form.errors,
+        }, status=422)
+
+
+class UpdateUser(LoginRequiredMixin, View):
+    def post(self, request: HttpRequest) -> JsonResponse:
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return SUCCESS_RESPONSE
+
         return JsonResponse(
             {
-                "status": "Method not allowed"
-            },
-            status=405
+                "status": "error",
+                "errors": form.errors,
+            }
         )
 
-    form = PasswordChangeForm(request.user, request.POST)
-    if form.is_valid():
-        user = form.save()
-        update_session_auth_hash(request, user)
-        return JsonResponse({
-            "status": "success",
-        }, status=200)
 
-    return JsonResponse(
-        {
-            "status": "error",
-            "errors": form.errors,
-        }, status=422
-    )
+class AddPowerExerciseView(LoginRequiredMixin, View):
+    def post(self, request: HttpRequest) -> JsonResponse:
+        exercise_id = request.POST.get("exercise_id", default=None)
+        training_id = request.POST.get("training_id", default=None)
+        if not exercise_id or not training_id:
+            return INVALID_DATA_RESPONSE
+
+        try:
+            exercise = Exercise.objects.get(pk=exercise_id)
+        except Exercise.DoesNotExist:
+            return NOT_FOUND_RESPONSE
+
+        try:
+            PowerTrainingExercise.objects.get(power_training_id=training_id,
+                                              exercise_id=exercise_id)
+            return INVALID_DATA_RESPONSE
+        except PowerTrainingExercise.DoesNotExist:
+            pass
+
+        power_training = PowerTrainingExercise.objects.create(exercise=exercise,
+                                                              power_training_id=training_id)
+        return JsonResponse(
+            {
+                "status": "success",
+                "power_training_id": power_training.id,
+            }
+        )
 
 
-@login_required
-def update_user(request: HttpRequest) -> JsonResponse:
-    if request.method != "POST":
-        return INVALID_METHOD_RESPONSE
+class AddApproachView(LoginRequiredMixin, View):
+    def post(self, request: HttpRequest) -> JsonResponse:
+        exercise_id = request.POST.get("power_training_exercise_id", default=None)
+        weight = request.POST.get("weight", default=None)
+        repeats = request.POST.get("repeats", default=None)
 
-    form = UserUpdateForm(request.POST, instance=request.user)
-    if form.is_valid():
-        form.save()
+        if not exercise_id:
+            return INVALID_DATA_RESPONSE
+
+        if not repeats:
+            return INVALID_DATA_RESPONSE
+
+        try:
+            power_exercise = PowerTrainingExercise.objects.get(pk=exercise_id)
+        except (Exercise.DoesNotExist, Approach.DoesNotExist):
+            return NOT_FOUND_RESPONSE
+
+        approach = Approach(training_id=power_exercise.id,
+                            repeats=repeats)
+        if weight:
+            approach.weight = weight
+
+        approach.save()
+
         return SUCCESS_RESPONSE
 
-    return JsonResponse(
-        {
-            "status": "error",
-            "errors": form.errors,
-        }
-    )
 
+class DeleteExerciseView(LoginRequiredMixin, View):
+    def post(self, request: HttpRequest) -> JsonResponse:
+        exercise_id = request.POST.get("exercise_id", default=None)
 
-@login_required
-def add_power_exercise(request: HttpRequest) -> JsonResponse:
-    if request.method != "POST":
-        return INVALID_METHOD_RESPONSE
+        if not exercise_id:
+            return INVALID_DATA_RESPONSE
 
-    exercise_id = request.POST.get("exercise_id", default=None)
-    training_id = request.POST.get("training_id", default=None)
-    if not exercise_id or not training_id:
-        return INVALID_DATA_RESPONSE
-
-    try:
-        exercise = Exercise.objects.get(pk=exercise_id)
-    except Exercise.DoesNotExist:
-        return NOT_FOUND_RESPONSE
-
-    try:
-        PowerTrainingExercise.objects.get(power_training_id=training_id, exercise_id=exercise_id)
-        return INVALID_DATA_RESPONSE
-    except PowerTrainingExercise.DoesNotExist:
-        pass
-
-    power_training = PowerTrainingExercise.objects.create(exercise=exercise,
-                                         power_training_id=training_id)
-    return JsonResponse(
-        {
-            "status": "success",
-            "power_training_id": power_training.id,
-        }
-    )
-
-
-@login_required
-def add_approach(request: HttpRequest) -> JsonResponse:
-    if request.method != "POST":
-        return INVALID_METHOD_RESPONSE
-
-    exercise_id = request.POST.get("power_training_exercise_id", default=None)
-    weight = request.POST.get("weight", default=None)
-    repeats = request.POST.get("repeats", default=None)
-
-    if not exercise_id:
-        return INVALID_DATA_RESPONSE
-
-    if not repeats:
-        return INVALID_DATA_RESPONSE
-
-    try:
-        power_exercise = PowerTrainingExercise.objects.get(pk=exercise_id)
-    except (Exercise.DoesNotExist, Approach.DoesNotExist):
-        return NOT_FOUND_RESPONSE
-
-    approach = Approach(training_id=power_exercise.id,
-                        repeats=repeats)
-    if weight:
-        approach.weight = weight
-
-    approach.save()
-
-    return SUCCESS_RESPONSE
-
-
-@login_required
-def delete_exercise(request: HttpRequest) -> JsonResponse:
-    if request.method != "POST":
-        return INVALID_METHOD_RESPONSE
-
-    exercise_id = request.POST.get("exercise_id", default=None)
-
-    if not exercise_id:
-        return INVALID_DATA_RESPONSE
-
-    try:
-        exercise = PowerTrainingExercise.objects.get(pk=exercise_id)
-        exercise.delete()
+        get_object_or_404(PowerTrainingExercise, pk=exercise_id).delete()
         return SUCCESS_RESPONSE
-    except Exercise.DoesNotExist:
-        return NOT_FOUND_RESPONSE
 
 
-def delete_approach(request: HttpRequest) -> JsonResponse:
-    if request.method != "POST":
-        return INVALID_METHOD_RESPONSE
+class DeleteApproach(LoginRequiredMixin, View):
+    def post(self, request: HttpRequest) -> JsonResponse:
+        approach_id = request.POST.get("approach_id", default=None)
+        if not approach_id:
+            return INVALID_DATA_RESPONSE
 
-    approach_id = request.POST.get("approach_id", default=None)
-    if not approach_id:
-        return INVALID_DATA_RESPONSE
-
-    try:
-        approach = Approach.objects.get(pk=approach_id)
-        approach.delete()
+        get_object_or_404(Approach, id=approach_id).delete()
         return SUCCESS_RESPONSE
-    except Approach.DoesNotExist:
-        return NOT_FOUND_RESPONSE
 
 
-def delete_training(request: HttpRequest) -> JsonResponse:
-    if request.method != "POST":
-        return INVALID_METHOD_RESPONSE
+class DeleteTrainingView(LoginRequiredMixin, View):
+    def post(self, request) -> JsonResponse:
+        training_type = request.POST.get("type", default=None)
+        training_id = request.POST.get("training_id", default=None)
+        if not training_type or not training_id:
+            return INVALID_DATA_RESPONSE
 
-    training_type = request.POST.get("type", default=None)
-    training_id = request.POST.get("training_id", default=None)
-    if not training_type or not training_id:
-        return INVALID_DATA_RESPONSE
-
-    try:
         if training_type == "PW":
-            training = PowerTrainingExercise.objects.get(pk=training_id)
-            training.delete()
+            get_object_or_404(PowerTraining, pk=training_id).delete()
         elif training_type == "CY":
-            training = CyclingTraining.objects.get(pk=training_id)
-            training.delete()
+            get_object_or_404(CyclingTraining, pk=training_id).delete()
         elif training_type == "SW":
-            training = SwimmingTraining.objects.get(pk=training_id)
-            training.delete()
+            get_object_or_404(SwimmingTraining, pk=training_id).delete()
         elif training_type == "WK":
-            training = Walk.objects.get(pk=training_id)
-            training.delete()
+            get_object_or_404(Walk, pk=training_id).delete()
         elif training_type == "JG":
-            training = Jogging.objects.get(pk=training_id)
-            training.delete()
+            get_object_or_404(Jogging, pk=training_id).delete()
         return SUCCESS_RESPONSE
-    except Exception:
-        return INVALID_DATA_RESPONSE
 
-
-def change_power_training_exercise(request: HttpRequest) -> JsonResponse:
-    if request.method != "POST":
-        return INVALID_METHOD_RESPONSE
-    #todo
