e525f3f1619c7a3b68ee6c16493ad200c5858205
AndrewwSSS <clash2clans1one@gmail.com>
2024-07-17 16:42:11 +0300

    refactoring and renaming in ajax/view, add DeleteExerciseView, DeleteDishView

--- ajax/views.py
+++ ajax/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import get_object_or_404
 from django.views import View
 
-from ajax.forms import UserUpdateForm
+from ajax.forms import UserUpdateForm, ApproachForm, DishCountForm
+from main.forms import ExerciseForm
 from main.models import (Exercise,
                          Approach,
                          PowerTrainingExercise,
@@ -31,7 +32,8 @@ INVALID_DATA_RESPONSE = JsonResponse({
 
 
 class UpdatePasswordView(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             user = form.save()
@@ -47,7 +49,8 @@ class UpdatePasswordView(LoginRequiredMixin, View):
 
 
 class UpdateUser(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
         form = UserUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
@@ -62,16 +65,15 @@ class UpdateUser(LoginRequiredMixin, View):
 
 
 class AddPowerExerciseView(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
         exercise_id = request.POST.get("exercise_id", default=None)
         training_id = request.POST.get("training_id", default=None)
+
         if not exercise_id or not training_id:
             return INVALID_DATA_RESPONSE
 
-        try:
-            exercise = Exercise.objects.get(pk=exercise_id)
-        except Exercise.DoesNotExist:
-            return NOT_FOUND_RESPONSE
+        exercise = get_object_or_404(Exercise, pk=exercise_id)
 
         try:
             PowerTrainingExercise.objects.get(power_training_id=training_id,
@@ -90,37 +92,23 @@ class AddPowerExerciseView(LoginRequiredMixin, View):
         )
 
 
-class AddApproachView(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        exercise_id = request.POST.get("power_training_exercise_id", default=None)
-        weight = request.POST.get("weight", default=None)
-        repeats = request.POST.get("repeats", default=None)
+class CreateApproachView(LoginRequiredMixin, View):
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
+        form = ApproachForm(request.POST, request=request)
 
-        if not exercise_id:
+        if form.is_valid():
+            approach = form.save()
+            return JsonResponse({
+                "approach_id": approach.id
+            }, status=200)
+        else:
             return INVALID_DATA_RESPONSE
 
-        if not repeats:
-            return INVALID_DATA_RESPONSE
 
-        try:
-            power_exercise = PowerTrainingExercise.objects.get(pk=exercise_id)
-        except (Exercise.DoesNotExist, Approach.DoesNotExist):
-            return NOT_FOUND_RESPONSE
-
-        approach = Approach(training_id=power_exercise.id,
-                            repeats=repeats)
-        if weight:
-            approach.weight = weight
-
-        approach.save()
-
-        return JsonResponse({
-            "approach_id": approach.id
-        }, status=200)
-
-
-class DeleteExerciseView(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
+class DeletePowerExerciseView(LoginRequiredMixin, View):
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
         exercise_id = request.POST.get("exercise_id",
                                        default=None)
         training_id = request.POST.get("training_id",
@@ -129,135 +117,136 @@ class DeleteExerciseView(LoginRequiredMixin, View):
         if not exercise_id or not training_id:
             return INVALID_DATA_RESPONSE
 
-        try:
-            training = PowerTraining.objects.get(pk=training_id,
-                                                 user=request.user)
-            exercise = training.exercises.get(pk=exercise_id)
-            exercise.delete()
-        except (PowerTraining.DoesNotExist, PowerTraining.DoesNotExist):
-            return NOT_FOUND_RESPONSE
+        get_object_or_404(PowerTrainingExercise,
+                          pk=exercise_id,
+                          power_training_id=training_id,
+                          power_training__user=request.user).delete()
         return SUCCESS_RESPONSE
 
 
 class DeleteApproach(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        approach_id = request.POST.get("approach_id", default=None)
-        if not approach_id:
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
+        approach = request.POST.get("approach_id", default=None)
+        exercise = request.POST.get("exercise_id", default=None)
+
+        if not approach or not exercise:
             return INVALID_DATA_RESPONSE
 
-        get_object_or_404(Approach, id=approach_id).delete()
+        get_object_or_404(Approach,
+                          id=approach,
+                          training__power_training__user_id=request.user.id,
+                          training_id=exercise).delete()
         return SUCCESS_RESPONSE
 
 
 class DeleteTrainingView(LoginRequiredMixin, View):
-    def post(self, request) -> JsonResponse:
+    @staticmethod
+    def post(request) -> JsonResponse:
         training_type = request.POST.get("type",
                                          default=None)
-        training_id = request.POST.get("training_id",
+        training_id = request.POST.get("id",
                                        default=None)
         if not training_type or not training_id:
             return INVALID_DATA_RESPONSE
 
         if training_type == "PW":
-            get_object_or_404(PowerTraining, pk=training_id).delete()
+            get_object_or_404(PowerTraining,
+                              pk=training_id,
+                              user=request.user).delete()
         elif training_type == "CY":
-            get_object_or_404(Cycling, pk=training_id).delete()
+            get_object_or_404(Cycling,
+                              pk=training_id,
+                              user=request.user).delete()
         elif training_type == "SW":
-            get_object_or_404(Swimming, pk=training_id).delete()
+            get_object_or_404(Swimming,
+                              pk=training_id,
+                              user=request.user).delete()
         elif training_type == "WK":
-            get_object_or_404(Walking, pk=training_id).delete()
+            get_object_or_404(Walking,
+                              pk=training_id,
+                              user=request.user).delete()
         elif training_type == "JG":
-            get_object_or_404(Jogging, pk=training_id).delete()
+            get_object_or_404(Jogging,
+                              pk=training_id,
+                              user=request.user).delete()
         return SUCCESS_RESPONSE
 
 
 class AddDishCountView(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        dish_id = request.POST.get("dish_id",
-                                   default=None)
-        meal_id = request.POST.get("meal_id",
-                                   default=None)
-        weight = request.POST.get("weight",
-                                  default=None)
-
-        if not dish_id or not meal_id or not weight:
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
+        form = DishCountForm(request.POST, user=request.user)
+        if form.is_valid():
+            dish_count = form.save()
+            return JsonResponse({
+                "dish_count_id": dish_count.id,
+            }, status=200)
+        else:
             return INVALID_DATA_RESPONSE
-
-        try:
-            dish = request.user.dishes.get(pk=dish_id)
-            meal = Meal.objects.get(pk=meal_id, user=request.user)
-        except (Dish.DoesNotExist, Meal.DoesNotExist):
-            return INVALID_DATA_RESPONSE
-
-        dish_count = DishCount.objects.create(dish=dish,
-                                              meal=meal,
-                                              weight=weight)
-
-        return JsonResponse({
-            "dish_count_id": dish_count.id,
-        }, status=200)
 
 
 class UpdateDishCountView(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        dish_id = request.POST.get("dish_id", default=None)
-        dish_count_id = request.POST.get("dish_count_id", default=None)
-        meal_id = request.POST.get("meal_id", default=None)
-        weight = request.POST.get("weight", default=None)
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
+        dish_count_id = request.POST.get("dish_count", default=None)
 
-        if not dish_id or not meal_id or not weight or not dish_count_id:
+        if not dish_count_id:
             return INVALID_DATA_RESPONSE
-
-        try:
-            meal = Meal.objects.get(pk=meal_id, user=request.user)
-            dish_count = meal.dishes.get(pk=dish_id)
-        except (Meal.DoesNotExist, DishCount.DoesNotExist):
+        dish_count = get_object_or_404(DishCount, pk=dish_count_id)
+        form = DishCountForm(request.POST,
+                             user=request.user,
+                             instance=dish_count)
+        if form.is_valid():
+            form.save()
+            return SUCCESS_RESPONSE
+        else:
             return INVALID_DATA_RESPONSE
-
-        dish_count.weight = weight
-        dish_count.save()
-        return SUCCESS_RESPONSE
 
 
 class DeleteDishCountView(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        dish_count_id = request.POST.get("dish_count_id", default=None)
-        meal_id = request.POST.get("meal_id", default=None)
-
-        if not dish_count_id or not meal_id:
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
+        dish_count_id = request.POST.get("dish_count", default=None)
+        if not dish_count_id:
             return INVALID_DATA_RESPONSE
-
-        try:
-            meal = Meal.objects.get(pk=meal_id, user=request.user)
-            dish_count = meal.dishes.get(pk=dish_count_id)
-        except (Meal.DoesNotExist, DishCount.DoesNotExist):
+        dish_count = get_object_or_404(DishCount, id=dish_count_id)
+        if dish_count.meal.user != request.user:
             return INVALID_DATA_RESPONSE
-
         dish_count.delete()
         return SUCCESS_RESPONSE
 
 
 class UpdateApproachView(LoginRequiredMixin, View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        approach_id = request.POST.get("approach_id", default=None)
-        repeats = request.POST.get("repeats", default=None)
-        weight = request.POST.get("weight", default=None)
-        exercise_id = request.POST.get("power_training_exercise_id", default=None)
-
-        if not approach_id or not exercise_id or (not repeats and not weight):
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
+        form = ApproachForm(request.POST, request=request)
+        if form.is_valid():
+            form.save()
+            return SUCCESS_RESPONSE
+        else:
             return INVALID_DATA_RESPONSE
 
-        try:
-            exercise = PowerTrainingExercise.objects.get(pk=exercise_id, power_training__user=request.user)
-            approach = exercise.approaches.get(pk=approach_id)
-        except (PowerTrainingExercise.DoesNotExist, Approach.DoesNotExist):
+
+class DeleteExerciseView(LoginRequiredMixin, View):
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
+        exercise_id = request.POST.get("exercise_id", default=None)
+
+        if not exercise_id:
             return INVALID_DATA_RESPONSE
 
-        if weight:
-            approach.weight = weight
+        get_object_or_404(Exercise, id=exercise_id).delete()
+        return SUCCESS_RESPONSE
 
-        if repeats:
-            approach.repeats = repeats
 
-        approach.save()
+class DeleteDishView(LoginRequiredMixin, View):
+    @staticmethod
+    def post(request: HttpRequest) -> JsonResponse:
+        dish_id = request.POST.get("dish_id", default=None)
+        if not dish_id:
+            return INVALID_DATA_RESPONSE
+        get_object_or_404(Dish,
+                          id=dish_id,
+                          user=request.user).delete()
         return SUCCESS_RESPONSE
