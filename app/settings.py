9a6e4cd5ec28164560af91bef4cedad6db317397
AndrewwSSS <clash2clans1one@gmail.com>
2024-07-22 14:04:22 +0300

    create debug switch construction from environment variables

--- app/settings.py
+++ app/settings.py
@@ -12,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_pbp2w1t48$(+l4v-!r^9#953+ix5uf)i-o*!u!%x)&o2g+24-'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-_pbp2w1t48$(+l4v-!r^9#953+ix5uf)i-o*!u!%x)&o2g+24-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["*"]
 
