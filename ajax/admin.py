3c6ff505457f9829046428c5272291762f5616bc
AndrewwSSS <clash2clans1one@gmail.com>
2024-07-23 11:02:04 +0300

    add admin panel

--- ajax/admin.py
+++ ajax/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import (User,
+                         PowerTraining,
+                         Cycling,
+                         Jogging,
+                         Walking,
+                         Swimming,
+                         Dish,
+                         Meal,
+                         Exercise,
+                         DishCount)
+
+admin.site.register(User)
+admin.site.register(PowerTraining)
+admin.site.register(Cycling)
+admin.site.register(Jogging)
+admin.site.register(Walking)
+admin.site.register(Swimming)
+admin.site.register(Dish)
+admin.site.register(Meal)
+admin.site.register(Exercise)
+admin.site.register(DishCount)
