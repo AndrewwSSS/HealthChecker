462c0ac9d2255259bde61d694bbb251b4d917076
AndrewwSSS <clash2clans1one@gmail.com>
2024-07-23 15:01:07 +0300

    change ALLOWED_HOSTS, add localhosts and main host

--- app/settings.py
+++ app/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "healthchecker.onrender.com"]
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
