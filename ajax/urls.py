0f8364fce94e353cb32cc4974a9921b942a923a6
AndrewwSSS <clash2clans1one@gmail.com>
2024-07-23 13:04:31 +0300

    add slashes to ends of get urls

--- ajax/urls.py
+++ ajax/urls.py
@@ -97,52 +97,52 @@ urlpatterns = [
         name="delete-meal"
     ),
     path(
-        "get_training_type_ratio",
+        "get_training_type_ratio/",
         GetTrainingsTypeRatioView.as_view(),
         name="get-training-type-ratio"
     ),
     path(
-        "get_avg_calories_info",
+        "get_avg_calories_info/",
         GetAvgCaloriesPerDayInfo.as_view(),
         name="get-calories-info"
     ),
     path(
-        "get_avg_protein_info",
+        "get_avg_protein_info/",
         GetAvgProteinPerDayView.as_view(),
         name="get-avg-protein-info"
     ),
     path(
-        "get_avg_carbohydrates_info",
+        "get_avg_carbohydrates_info/",
         GetAvgCarbohydratesPerDayView.as_view(),
         name="get-avg-carbohydrates-info"
     ),
     path(
-        "get_avg_fats_info",
+        "get_avg_fats_info/",
         GetAvgFatsPerDayView.as_view(),
         name="get-avg-fats-info"
     ),
     path(
-        "get_total_km_by_cycling",
+        "get_total_km_by_cycling/",
         GetTotalKMbyCycling.as_view(),
         name="get-total-km-by-cycling"
     ),
     path(
-        "get_total_km_by_jogging",
+        "get_total_km_by_jogging/",
         GetTotalKMbyJogging.as_view(),
         name="get-total-km-by-jogging"
     ),
     path(
-        "get_total_km_by_walking",
+        "get_total_km_by_walking/",
         GetTotalKMbyWalking.as_view(),
         name="get-total-km-by-walking"
     ),
     path(
-        "get_total_km_by_swimming",
+        "get_total_km_by_swimming/",
         GetTotalKMbySwimming.as_view(),
         name="get-total-km-by-swimming"
     ),
     path(
-        "get_pfc_ratio",
+        "get_pfc_ratio/",
         GetPFCratio.as_view(),
         name="get-pfc-ratio"
     ),
